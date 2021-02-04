4ee8a5d834bce25105d465aa98cad19c0ff27168
chou0728 <dean34520@gmail.com>
2021-02-04 16:09:30 +0800

    replace createConnection to createPool

--- database.js
+++ database.js
@@ -1,17 +1,37 @@
 const dotenv = require('dotenv');
+const mysql = require('mysql');
+
 dotenv.config({
   path:
     process.env.NODE_ENV === 'development'
       ? './.env.development.local'
       : './.env.production.local'
 });
-const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-module.exports = connection;
+const query = (sql, values) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err);
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+          connection.release();
+        });
+      }
+    });
+  });
+};
+
+module.exports = { query };
